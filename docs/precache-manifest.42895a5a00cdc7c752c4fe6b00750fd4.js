9f307bd766918d10304186db4531431ccebada85
Andrei <adanciu@viasatelematics.com>
2020-07-23 15:42:25 +0200

    First

---
+++ docs/precache-manifest.42895a5a00cdc7c752c4fe6b00750fd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10fbf7024888a6afdf0bb9be5afca7a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a87bb1260d602abfd6ef",
+    "url": "/static/css/main.b53daf71.chunk.css"
+  },
+  {
+    "revision": "450c009cbe237af53050",
+    "url": "/static/js/2.224915d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.224915d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a87bb1260d602abfd6ef",
+    "url": "/static/js/main.27d33368.chunk.js"
+  },
+  {
+    "revision": "abccdcd76010c2c1074c",
+    "url": "/static/js/runtime-main.9897510a.js"
+  }
+]);
